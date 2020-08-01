f0386a8bda19b6512ca07461313f09c02a52ca33
unknown <ed.f.jones@gmail.com>
2020-08-01 14:39:47 -0400

    added contacts and started styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,34 @@ import React from 'react'
     // background pictures needs to be added.
     return (
         <section className="section-contact">
-            hello from contact me page
+            <div classNmae="contact-container">
+                <div>
+                    <div>
+                        <form>
+                            <div>
+                                <input type="text" id="firstname" placeholder="Firstname">
+                                </input>
+                            </div>
+
+                            <div>
+                                <input type="text" id="lastname" placeholder="Lastname">
+                                </input>
+                            </div>
+
+                            <div>
+                                <input type="email" id="email" placeholder="email">
+                                </input>
+                            </div>
+                            <div>
+                                <textarea palceholder="Enter meassage here...">
+
+                                </textarea>
+                            </div>
+                            <button>submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
