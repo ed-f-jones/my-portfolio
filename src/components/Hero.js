6c0445724b442063e9b312dd0f41fe191f5bf94e
unknown <ed.f.jones@gmail.com>
2020-08-06 15:33:40 -0400

    added navbar and css styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 import '../App.css';
 
  function Hero() {
@@ -7,6 +8,7 @@ import '../App.css';
      // link all sections together with smooth scrolling
     return (
         <header className="hero" id="home">
+            <Navbar/>
             <div className="hero-title">
                 <h1  data-aos="fade-left" data-aos-duration="1500">Edward F. Jones</h1>
                 <h2 data-aos="fade-right" data-aos-duration="2500" >Web Developer</h2>
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><a href="#home">home</a></li>
+                <li><a href="about">about</a></li>
+                <li><a href="portfolio">portfolio</a></li>
+                <li><a href="contact"> contact</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar;
